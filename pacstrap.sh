0d5b65687ae40f163d9088127b485b4937eaacb7
kub4e <61429531+kub4e@users.noreply.github.com>
2020-09-20 19:15:02 +0300

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,1 +1,1 @@
-pacstrap /mnt base base-devel linux linux-firmware efibootmgr networkmanager zsh texinfo vim git man-db man-pages wget
+pacstrap /mnt base base-devel linux linux-firmware efibootmgr networkmanager zsh texinfo vim git man-db man-pages wget sudo dhcpcd dhclient lvm2
