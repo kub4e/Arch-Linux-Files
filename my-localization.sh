8afc7497a5bde3b00a1fbc8826290e9a3796a36e
kub4e <61429531+kub4e@users.noreply.github.com>
2020-09-20 19:57:11 +0300

    Update my-localization.sh

--- my-localization.sh
+++ my-localization.sh
@@ -1,14 +1,26 @@
 ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime
-sleep 2
+sleep 1
 
 hwclock --systohc
-sleep 2
+sleep 1
 
 cat /dev/null > /etc/locale.gen
 sleep 1
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-sleep 2
+sleep 1
 
 locale-gen
-sleep 2
+sleep 1
+
+cat /dev/null > /etc/hosts
+sleep 1
+
+cat hosts > /etc/hosts
+sleep 1
+
+echo down > /etc/hostname
+sleep 1
+
+systemctl enable NetworkManager
+sleep 1
